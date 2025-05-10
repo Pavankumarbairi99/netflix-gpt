6cb7ba056a3ae19877eda6e3aa7f4d90d6e2bd01
Pavan Kumar <bairypawan99@gmail.com>
2025-05-11 00:54:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-xl font-bold text-center my-5 text-red-700'>Hello Namaste Reactjs</h1>
+    </div>
+  );
+}
+
+export default App;
